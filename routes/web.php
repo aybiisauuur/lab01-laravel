8288a83b9d94c6b5f40abf41fc65c4069e5203ff
aybiisauuur <ivyaquino981@gmail.com>
2025-03-12 08:40:51 +0800

    hindi gumagana!!

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController; // Add this line to import the NoteController
+use App\Http\Controllers\UserController;
 
 // Default route
 Route::get('/', function () {
@@ -10,3 +11,9 @@ Route::get('/', function () {
 
 // Add this for the NoteController resource routes
 Route::resource('notes', NoteController::class);
+
+Route::get('/register', [UserController::class, 'showRegistrationForm']);
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+Route::get('/login', [UserController::class, 'showLoginForm']);
+Route::post('/login', [UserController::class, 'login'])->name('login');
